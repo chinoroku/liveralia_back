bce73ed88424798910ec0d348572c13cdc723697
chinoroku <angelocaveri@gmail.com>
2023-12-27 16:23:52 -0500

    mercado-pago

--- routes/customer.js
+++ routes/customer.js
@@ -12,5 +12,9 @@ api.delete('/eliminar_producto_carrito/:id',cauthenticate.decodeToken, customerC
 api.post('/crear_direccion_cliente',cauthenticate.decodeToken, customerController.crear_direccion_cliente);
 api.get('/obternet_direcciones_cliente',cauthenticate.decodeToken, customerController.obternet_direcciones_cliente);
 api.delete('/eliminar_direccion_cliente/:id',cauthenticate.decodeToken, customerController.eliminar_direccion_cliente);
+api.get('/validar_payment_id_venta/:payment_id',cauthenticate.decodeToken,customerController.validar_payment_id_venta);
+api.post('/crear_venta_cliente',cauthenticate.decodeToken, customerController.crear_venta_cliente);
+api.get('/obtener_informacion_venta/:id',cauthenticate.decodeToken,customerController.obtener_informacion_venta);
+api.get('/obtener_ventas_clientes',cauthenticate.decodeToken,customerController.obtener_ventas_clientes);
 
 module.exports = api;
