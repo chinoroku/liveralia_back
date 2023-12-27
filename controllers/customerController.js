abbe0262183232b2152afe4efe6a8c0be3056209
chinoroku <angelocaveri@gmail.com>
2023-12-27 13:35:32 -0500

    ventas

--- controllers/customerController.js
+++ controllers/customerController.js
@@ -1,5 +1,6 @@
 var Carrito = require('../models/carrito');
 var Variedad = require('../models/variedad');
+var Direccion = require('../models/direccion');
 
 const crear_producto_carrito = async function(req,res){
     if(req.user){
@@ -34,7 +35,53 @@ const obtener_carrito_cliente = async function(req,res){
     }
 }
 
+const eliminar_producto_carrito = async function(req,res){
+    if(req.user){
+       let id = req.params['id'];
+       let reg = await Carrito.findByIdAndRemove({_id:id});
+       res.status(200).send(reg);
+    }else{
+        res.status(500).send({data:undefined,message: 'ErrorToken'});
+    }
+}
+
+const crear_direccion_cliente = async function(req,res){
+    if(req.user){
+       let data = req.body;
+       data.cliente = req.user.sub;
+       let direccion = await Direccion.create(data);
+       res.status(200).send(direccion);
+    }else{
+        res.status(500).send({data:undefined,message: 'ErrorToken'});
+    }
+}
+
+const obternet_direcciones_cliente = async function(req,res){
+    if(req.user){
+      
+       let direcciones = await Direccion.find({cliente:req.user.sub});
+       res.status(200).send(direcciones);
+    }else{
+        res.status(500).send({data:undefined,message: 'ErrorToken'});
+    }
+}
+
+const eliminar_direccion_cliente = async function(req,res){
+    if(req.user){
+       let id = req.params['id'];
+       let direccion = await Direccion.findOneAndDelete({_id:id});
+       res.status(200).send(direccion);
+    }else{
+        res.status(500).send({data:undefined,message: 'ErrorToken'});
+    }
+}
+
+
 module.exports = {
     crear_producto_carrito,
-    obtener_carrito_cliente
+    obtener_carrito_cliente,
+    eliminar_producto_carrito,
+    crear_direccion_cliente,
+    obternet_direcciones_cliente,
+    eliminar_direccion_cliente
 }
--- routes/customer.js
+++ routes/customer.js
@@ -6,5 +6,11 @@ var api = express.Router();
 
 api.post('/crear_producto_carrito',cauthenticate.decodeToken, customerController.crear_producto_carrito);
 api.get('/obtener_carrito_cliente',cauthenticate.decodeToken, customerController.obtener_carrito_cliente);
+api.delete('/eliminar_producto_carrito/:id',cauthenticate.decodeToken, customerController.eliminar_producto_carrito);
+
+
+api.post('/crear_direccion_cliente',cauthenticate.decodeToken, customerController.crear_direccion_cliente);
+api.get('/obternet_direcciones_cliente',cauthenticate.decodeToken, customerController.obternet_direcciones_cliente);
+api.delete('/eliminar_direccion_cliente/:id',cauthenticate.decodeToken, customerController.eliminar_direccion_cliente);
 
 module.exports = api;
