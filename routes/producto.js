899e2c4fb161e06c74c06ab5ec4559f3bdc1feca
rios <riosarone99@gmail.com>
2023-12-27 13:33:15 -0500

    galeria-producto-back

--- routes/producto.js
+++ routes/producto.js
@@ -5,6 +5,7 @@ var multipart = require('connect-multiparty');
 
 var path = multipart({uploadDir: './uploads/productos'});
 var path_ingreso = multipart({uploadDir: './uploads/facturas'});
+var path_galeria = multipart({uploadDir: './uploads/galeria'});
 var api = express.Router();
 
 api.post('/registro_producto_admin', [authenticate.decodeToken, path], productoController.registro_producto_admin);
@@ -31,5 +32,9 @@ api.put('/cambiar_estado_producto_admin/:id',authenticate.decodeToken, productoC
 //////////////////////////////////////////////////////////////////
 api.post('/registro_ingreso_admin',[authenticate.decodeToken, path_ingreso], productoController.registro_ingreso_admin);
 
-
+//////////////////////////////////////////////////////////////////
+api.post('/subir_imagen_producto_admin', [authenticate.decodeToken, path_galeria], productoController.subir_imagen_producto_admin);
+api.get('/obtener_galeria_producto/:img', productoController.obtener_galeria_producto);
+api.get('/obtener_galeria_producto_admin/:id', authenticate.decodeToken, productoController.obtener_galeria_producto_admin);
+api.delete('/eliminar_galeria_producto_admin/:id', authenticate.decodeToken, productoController.eliminar_galeria_producto_admin);
 module.exports = api;
